8c259684adc4afc7d1c34b5c88b0d04a194cd7cc
SMILE-KILLER10 <85184491+SMILE-KILLER10@users.noreply.github.com>
2021-06-19 11:57:27 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -142,7 +142,7 @@ async def cb_navg(bot, update: CallbackQuery):
     
     reply_markup = InlineKeyboardMarkup(temp_results)
     
-    text=f"<i>Found</i> <code>{leng}</code> <i>Results For Your Request:</i> <code>{query}</code>"
+    text=f"<i>JOIN @FC_FILM_CORNER.Results For Your Request:</i> <code>{query}</code>"
         
     try:
         await update.message.edit(
